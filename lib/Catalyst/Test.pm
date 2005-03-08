0b944e631d7ef031353cf662671dea98095e43c7
Sebastian Riedel <sri@labs.kraih.com>
2005-03-08 19:44:14 +0000

    for draven

--- lib/Catalyst/Test.pm
+++ lib/Catalyst/Test.pm
@@ -61,7 +61,8 @@ sub import {
     my $self = shift;
     $class = shift;
     $class->require;
-    if ( ( caller(0) )[1] eq '-e' ) {
+    my $caller = ( caller(0) )[1];
+    unless ( $INC{'Test/Builder.pm'} ) {
         die qq/Couldn't load "$class", "$@"/ if $@;
     }
     my $caller = caller(0);
