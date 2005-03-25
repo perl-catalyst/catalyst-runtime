5a8fd7e9d4cca2af0237e1659f061cc64618bfe6
Sebastian Riedel <sri@labs.kraih.com>
2005-03-25 15:08:27 +0000

    added Test::Pod support

--- lib/Catalyst/Helper.pm
+++ lib/Catalyst/Helper.pm
@@ -308,7 +308,16 @@ use_ok( Catalyst::Test, '$name' );
 
 ok( request('/')->is_success );
 EOF
-    $self->mk_file( "$t\/02podcoverage.t", <<"EOF");
+    $self->mk_file( "$t\/02pod.t", <<"EOF");
+use Test::More;
+
+eval "use Test::Pod 1.14";
+plan skip_all => 'Test::Pod 1.14 required' if $@;
+plan skip_all => 'set TEST_POD to enable this test' unless $ENV{TEST_POD};
+
+all_pod_files_ok();
+EOF
+    $self->mk_file( "$t\/03podcoverage.t", <<"EOF");
 use Test::More;
 
 eval "use Test::Pod::Coverage 1.04";
