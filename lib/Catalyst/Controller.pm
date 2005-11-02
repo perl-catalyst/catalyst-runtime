5ee249f25d549a71fdb509af9d99151f62cddab8
Sebastian Riedel <sri@labs.kraih.com>
2005-11-02 01:34:34 +0000

    Added Catalyst::Controller, Catalyst::Model and Catalyst::View base classes

---
+++ lib/Catalyst/Controller.pm
@@ -0,0 +1,29 @@
+package Catalyst::Controller;
+
+use strict;
+use base qw/Catalyst::Base/;
+
+=head1 NAME
+
+Catalyst::Controller - Catalyst Controller base class
+
+=head1 SYNOPSIS
+
+See L<Catalyst>.
+
+=head1 DESCRIPTION
+
+Catalyst Controller base class.
+
+=head1 AUTHOR
+
+Sebastian Riedel, C<sri@oook.de>
+
+=head1 COPYRIGHT
+
+This program is free software, you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
+
+1;
---
+++ lib/Catalyst/View.pm
@@ -0,0 +1,29 @@
+package Catalyst::View;
+
+use strict;
+use base qw/Catalyst::Component/;
+
+=head1 NAME
+
+Catalyst::View - Catalyst View base class
+
+=head1 SYNOPSIS
+
+See L<Catalyst>.
+
+=head1 DESCRIPTION
+
+Catalyst View base class.
+
+=head1 AUTHOR
+
+Sebastian Riedel, C<sri@oook.de>
+
+=head1 COPYRIGHT
+
+This program is free software, you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
+
+1;
