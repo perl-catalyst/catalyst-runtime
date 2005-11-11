198b0efa0739da9df2e79ac23ea80b63aa651db8
Andy Grundman <andy@hybridized.org>
2005-11-11 21:00:15 +0000

    Added some docs and example configs for FastCGI with Apache and lighttpd

--- lib/Catalyst/Engine/FastCGI.pm
+++ lib/Catalyst/Engine/FastCGI.pm
@@ -100,7 +100,84 @@ sub write {
     *STDOUT->syswrite($buffer);
 }
 
+1;
+__END__
+
 =back
+
+=head1 WEB SERVER CONFIGURATIONS
+
+=head2 Apache 1.x, 2.x
+
+Apache requires the mod_fastcgi module.  The following config will let Apache
+control the running of your FastCGI processes.
+
+    # Launch the FastCGI processes
+    FastCgiIpcDir /tmp
+    FastCgiServer /var/www/MyApp/script/myapp_fastcgi.pl -idle_timeout 300 -processes 5
+    
+    <VirtualHost *>
+        ScriptAlias / /var/www/MyApp/script/myapp_fastcgi.pl/
+    </VirtualHost>
+    
+You can also tell Apache to connect to an external FastCGI server:
+
+    # Start the external server (requires FCGI::ProcManager)
+    $ script/myapp_fastcgi.pl -l /tmp/myapp.socket -n 5
+    
+    # Note that the path used in FastCgiExternalServer can be any path
+    FastCgiIpcDir /tmp
+    FastCgiExternalServer /tmp/myapp_fastcgi.pl -socket /tmp/myapp.socket
+    
+    <VirtualHost *>
+        ScriptAlias / /tmp/myapp_fastcgi.pl/
+    </VirtualHost>
+    
+For more information on using FastCGI under Apache, visit
+L<http://www.fastcgi.com/mod_fastcgi/docs/mod_fastcgi.html>
+
+=head2 Lighttpd
+
+This configuration was tested with Lighttpd 1.4.7.
+
+    server.document-root = "/var/www/MyApp/root"
+    
+    fastcgi.server = (
+        "" => (
+            "MyApp" => (
+                "socket"       => "/tmp/myapp.socket",
+                "check-local"  => "disable",
+                "bin-path"     => "/var/www/MyApp/script/myapp_fastcgi.pl",
+                "min-procs"    => 2,
+                "max-procs"    => 5,
+                "idle-timeout" => 20
+            )
+        )
+    )
+    
+Or use an external server:
+
+    # Start the external server (requires FCGI::ProcManager)
+    $ script/myapp_fastcgi.pl -l /tmp/myapp.socket -n 5
+
+    server.document-root = "/var/www/MyApp/root"
+
+    fastcgi.server = (
+        "" => (
+            "MyApp" => (
+                "socket"      => "/tmp/myapp.socket",
+                "check-local" => "disable"
+            )
+        )
+    )
+
+For more information on using FastCGI under Lighttpd, visit
+L<http://www.lighttpd.net/documentation/fastcgi.html>
+
+=head2 IIS
+
+It is possible to run Catalyst under IIS with FastCGI, but we do not
+yet have detailed instructions.
 
 =head1 SEE ALSO
 
@@ -120,5 +197,3 @@ This program is free software, you can redistribute it and/or modify it under
 the same terms as Perl itself.
 
 =cut
-
-1;
