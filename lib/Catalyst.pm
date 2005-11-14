ff2360486d5ce8aa0a9db80daec8c0c2a4b02fa7
Yuval Kogman <nothingmuch@woobling.org>
2005-11-14 18:04:14 +0000

    Make headers and cookies non-writable after finalize-headers

--- lib/Catalyst.pm
+++ lib/Catalyst.pm
@@ -16,6 +16,8 @@ use Path::Class;
 use Time::HiRes qw/gettimeofday tv_interval/;
 use URI;
 use Scalar::Util qw/weaken/;
+use Hash::Util qw/lock_hash/;
+use HTTP::Headers::ReadOnly;
 use attributes;
 
 __PACKAGE__->mk_accessors(
@@ -1023,7 +1025,11 @@ Finalizes cookies.
 
 =cut
 
-sub finalize_cookies { my $c = shift; $c->engine->finalize_cookies( $c, @_ ) }
+sub finalize_cookies {
+	my $c = shift;
+	$c->engine->finalize_cookies( $c, @_ );
+	lock_hash( %$_ ) for $c->res->cookies, values %{ $c->res->cookies };
+}
 
 =item $c->finalize_error
 
@@ -1065,6 +1071,8 @@ sub finalize_headers {
     $c->finalize_cookies;
 
     $c->engine->finalize_headers( $c, @_ );
+
+	bless $c->response->headers, "HTTP::Headers::ReadOnly";
 
     # Done
     $c->response->{_finalized_headers} = 1;
@@ -1773,6 +1781,9 @@ Returns the stack.
 Writes $data to the output stream. When using this method directly, you
 will need to manually set the C<Content-Length> header to the length of
 your output data, if known.
+
+Also note that any headers created after the write can  no longer be added, and
+this includes cookies.
 
 =cut
 
