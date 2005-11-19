50cc3183118f1056427ab314b84b91fd0d9e1383
Sebastian Riedel <sri@labs.kraih.com>
2005-11-19 05:38:02 +0000

    Renamed tests

--- t/optional_apache-cgi.pl
+++ t/optional_apache-cgi.pl
@@ -4,7 +4,7 @@
 #
 # Note, to get this to run properly, you may need to give it the path to your
 # httpd.conf:
-# 
+#
 # perl t/optional/apache-cgi.pl -httpd_conf /etc/apache/httpd.conf
 
 use strict;
@@ -19,14 +19,14 @@ use FindBin;
 use IO::Socket;
 
 # clean up
-rmtree "$FindBin::Bin/../../t/tmp" if -d "$FindBin::Bin/../../t/tmp";
+rmtree "$FindBin::Bin/../t/tmp" if -d "$FindBin::Bin/../t/tmp";
 
 # create a TestApp and copy the test libs into it
-mkdir "$FindBin::Bin/../../t/tmp";
-chdir "$FindBin::Bin/../../t/tmp";
-system "$FindBin::Bin/../../script/catalyst.pl TestApp";
-chdir "$FindBin::Bin/../..";
-File::Copy::Recursive::dircopy( 't/live/lib', 't/tmp/TestApp/lib' );
+mkdir "$FindBin::Bin/../t/tmp";
+chdir "$FindBin::Bin/../t/tmp";
+system "$FindBin::Bin/../script/catalyst.pl TestApp";
+chdir "$FindBin::Bin/..";
+File::Copy::Recursive::dircopy( 't/lib', 't/tmp/TestApp/lib' );
 
 # remove TestApp's tests so Apache::Test doesn't try to run them
 rmtree 't/tmp/TestApp/t';
@@ -37,8 +37,8 @@ Apache::TestRun->new->run(@ARGV);
 
 # clean up if the server has shut down
 # this allows the test files to stay around if the user ran -start-httpd
-if ( ! check_port( 'localhost', 8529 ) ) {
-    rmtree "$FindBin::Bin/../../t/tmp" if -d "$FindBin::Bin/../../t/tmp";
+if ( !check_port( 'localhost', 8529 ) ) {
+    rmtree "$FindBin::Bin/../t/tmp" if -d "$FindBin::Bin/../t/tmp";
 }
 
 sub check_port {
