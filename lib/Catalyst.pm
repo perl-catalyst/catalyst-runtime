270b09227394de789946bd18835d6f0284d79150
Andy Grundman <andy@hybridized.org>
2006-01-17 19:20:19 +0000

    Back to 6.00 at sri's request

--- lib/Catalyst.pm
+++ lib/Catalyst.pm
@@ -59,7 +59,7 @@ __PACKAGE__->engine_class('Catalyst::Engine::CGI');
 __PACKAGE__->request_class('Catalyst::Request');
 __PACKAGE__->response_class('Catalyst::Response');
 
-our $VERSION = '5.62';
+our $VERSION = '6.00';
 
 sub import {
     my ( $class, @arguments ) = @_;
