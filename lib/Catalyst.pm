50f6a990e35f22a9037b1caf72bb8271bb556e78
Andy Grundman <andy@hybridized.org>
2006-05-04 16:50:45 +0000

    5.6902 release

--- lib/Catalyst.pm
+++ lib/Catalyst.pm
@@ -61,7 +61,7 @@ __PACKAGE__->engine_class('Catalyst::Engine::CGI');
 __PACKAGE__->request_class('Catalyst::Request');
 __PACKAGE__->response_class('Catalyst::Response');
 
-our $VERSION = '5.6901';
+our $VERSION = '5.6902';
 
 sub import {
     my ( $class, @arguments ) = @_;
