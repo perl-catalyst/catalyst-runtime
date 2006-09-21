8df786038087f3c31a62dc7163c81b1f69270447
Marcus Ramberg <marcus@nordaaker.com>
2006-09-21 14:33:30 +0000

    prepared for 03

--- lib/Catalyst.pm
+++ lib/Catalyst.pm
@@ -61,7 +61,7 @@ __PACKAGE__->response_class('Catalyst::Response');
 
 # Remember to update this in Catalyst::Runtime as well!
 
-our $VERSION = '5.7002';
+our $VERSION = '5.7003';
 
 sub import {
     my ( $class, @arguments ) = @_;
