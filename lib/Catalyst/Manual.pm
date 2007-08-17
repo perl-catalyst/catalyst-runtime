c8870b8c94c51b11c52d131e3e8c0ac13fd9f28e
Kieren Diment <diment@gmail.com>
2007-08-17 13:54:31 +0000

    clarifying where Catalyst::Manual::* is

--- lib/Catalyst/Manual.pm
+++ lib/Catalyst/Manual.pm
@@ -9,6 +9,17 @@ reference for Catalyst.
 
 =over 4
 
+=head1 IMPORTANT NOTE
+
+If you need to read the Catalyst Manual make sure that you have
+Catalyst::Manual installed from cpan.  To check that it is installed run the following command from a unix (bash) prompt:
+
+ $ perldoc -t Catalyst::Manual::Tutorial::CatalystBasics 2>&1 >/dev/null && echo OK || echo MISSING
+
+If you see "OK" as the output, it's there, if you see "MISSING" you
+need to install the
+L<Catalyst::Manual|http://search.cpan.org/search?query=Catalyst%3A%3AManual&mode=dist> distribution.
+
 =item *
 
 L<Catalyst::Manual::About>
