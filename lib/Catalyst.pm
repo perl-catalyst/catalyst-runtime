228f9466174380edff95b369034a04fd4b9cbb82
Andy Grundman <andy@hybridized.org>
2007-10-10 15:41:18 +0000

    Removed unnecessary sprintf in debug mode that caused warnings on locales where commas are used for decimal markers.

--- lib/Catalyst.pm
+++ lib/Catalyst.pm
@@ -1353,7 +1353,7 @@ sub finalize {
     }
     
     if ($c->debug) {
-        my $elapsed = sprintf '%f', tv_interval($c->stats->getNodeValue);
+        my $elapsed = tv_interval($c->stats->getNodeValue);
         my $av = $elapsed == 0 ? '??' : sprintf '%.3f', 1 / $elapsed;
         
         my $t = Text::SimpleTable->new( [ 62, 'Action' ], [ 9, 'Time' ] );
