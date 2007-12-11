ea52914e91ed87fb3411eec88c996d537b797247
Andy Grundman <andy@hybridized.org>
2007-12-11 14:17:58 +0000

    Fixed a bug where -rr (restart regex) command line option could cause shell errors

--- lib/Catalyst/Engine/HTTP.pm
+++ lib/Catalyst/Engine/HTTP.pm
@@ -333,7 +333,7 @@ sub run {
         use Config;
         $ENV{PERL5LIB} .= join $Config{path_sep}, @INC; 
         
-        exec $^X . ' "' . $0 . '" ' . join( ' ', @{ $options->{argv} } );
+        exec $^X, $0, @{ $options->{argv} };
     }
 
     exit;
