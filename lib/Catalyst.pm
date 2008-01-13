221cd1f58a0cdea3d91c874f1940a1c06df3fb76
Jonathan Rockway <jon@jrock.us>
2008-01-13 22:45:35 +0000

    encode unicode to utf8 by default (in finalize)

--- lib/Catalyst.pm
+++ lib/Catalyst.pm
@@ -1335,6 +1335,11 @@ sub finalize {
         $c->log->error($error);
     }
 
+    # utf8-encode the body (convert perl chars to utf8 on the wire)
+    if ( $c->response->{body} && utf8::is_utf8($c->response->{body}) ){
+        utf8::encode( $c->response->{body} );
+    }
+    
     # Allow engine to handle finalize flow (for POE)
     if ( $c->engine->can('finalize') ) {
         $c->engine->finalize($c);
