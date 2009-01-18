e9f1c159da06d12087ea77c429197c3d9b8e3c11
Marcus Ramberg <marcus@nordaaker.com>
2009-01-18 21:39:59 +0000

    Prepare for 5.71000 release

--- lib/Catalyst.pm
+++ lib/Catalyst.pm
@@ -64,7 +64,7 @@ __PACKAGE__->stats_class('Catalyst::Stats');
 
 # Remember to update this in Catalyst::Runtime as well!
 
-our $VERSION = '5.7099_04';
+our $VERSION = '5.71000';
 
 sub import {
     my ( $class, @arguments ) = @_;
--- lib/Catalyst/Runtime.pm
+++ lib/Catalyst/Runtime.pm
@@ -7,7 +7,7 @@ BEGIN { require 5.008001; }
 
 # Remember to update this in Catalyst as well!
 
-our $VERSION='5.7099_04';
+our $VERSION='5.71000';
 
 $VERSION= eval $VERSION; 
 
