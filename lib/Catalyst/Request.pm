8f58057d45b3caf4f025985390f1faaa61302851
Florian Ragwitz <rafl@debian.org>
2009-02-05 14:01:11 +0000

    Clarify that request arguments aren't unescaped automatically.
    Courtesy of Simon Bertrang. Closes RT#41153.

--- lib/Catalyst/Request.pm
+++ lib/Catalyst/Request.pm
@@ -210,6 +210,9 @@ For example, if your action was
 and the URI for the request was C<http://.../foo/moose/bah>, the string C<bah>
 would be the first and only argument.
 
+Arguments just get passed through and B<don't> get unescaped automatically, so
+you should do that explicitly.
+
 =head2 $req->args
 
 Shortcut for arguments.
