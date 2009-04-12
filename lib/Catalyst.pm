b6103f13426520d083ef9e0671dbc6ea2750cd40
Marcus Ramberg <marcus@nordaaker.com>
2009-04-12 17:38:59 +0000

    Prepare for maybe-release

--- lib/Catalyst.pm
+++ lib/Catalyst.pm
@@ -76,7 +76,7 @@ __PACKAGE__->stats_class('Catalyst::Stats');
 
 # Remember to update this in Catalyst::Runtime as well!
 
-our $VERSION = '5.8000_06';
+our $VERSION = '5.8000_07';
 
 {
     my $dev_version = $VERSION =~ /_\d{2}$/;
--- lib/Catalyst/Runtime.pm
+++ lib/Catalyst/Runtime.pm
@@ -7,7 +7,7 @@ BEGIN { require 5.008001; }
 
 # Remember to update this in Catalyst as well!
 
-our $VERSION='5.8000_06';
+our $VERSION='5.8000_07';
 
 $VERSION = eval $VERSION;
 
