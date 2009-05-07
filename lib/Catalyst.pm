123c73194d1d4e630b17f211b1fbfd2edeff190b
Tomas Doran <bobtfish@bobtfish.net>
2009-05-07 00:13:23 +0000

    Back out last change. vincent worked out it's a bug in MOP

--- lib/Catalyst.pm
+++ lib/Catalyst.pm
@@ -1100,7 +1100,6 @@ EOF
     # modifiers work correctly in MyApp (as you have to call setup _before_
     # applying modifiers).
     Scope::Upper::reap(sub {
-        die $@ if $@; # This does not appear to do what you think it does.
         my $meta = Class::MOP::get_metaclass_by_name($class);
         if ( $meta->is_immutable && ! { $meta->immutable_options }->{inline_constructor} ) {
             die "You made your application class ($class) immutable, "
