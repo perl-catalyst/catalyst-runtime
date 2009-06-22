ddd00df4aedc3a55e04a9ef5109103ebfa8bba4c
Rafael Kitover <rkitover@io.com>
2009-06-22 02:17:29 +0000

    missing / in test file

--- t/lib/TestAppIndexDefault/Controller/Default.pm
+++ t/lib/TestAppIndexDefault/Controller/Default.pm
@@ -7,7 +7,7 @@ sub default : Private {
     $c->res->body('default_default');
 }
 
-sub path_one_arg : Path('/default') Args(1) {
+sub path_one_arg : Path('/default/') Args(1) {
     my ($self, $c) = @_;
     $c->res->body('default_path_one_arg');
 }
