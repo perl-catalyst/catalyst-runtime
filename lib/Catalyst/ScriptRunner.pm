d24d92d9ed09f3cbd48356058429849e7c66ed73
Devin Austin <devin.austin@gmail.com>
2009-06-26 19:46:17 +0000

    updated so that if their class doesn't load, we use ours.

--- lib/Catalyst/ScriptRunner.pm
+++ lib/Catalyst/ScriptRunner.pm
@@ -4,7 +4,12 @@ use Moose;
 sub run {
     my ($self, $class, $scriptclass) = @_;
     my $classtoload = "${class}::Script::$scriptclass"; 
-    Class::MOP::load_class($classtoload); 
-    $classtoload->new_with_options->run;
+    
+    if ( Class::MOP::load_class($classtoload) ) {  
+        $classtoload->new_with_options->run;
+    } else {
+        $classtoload = "Catalyst::Script::$scriptclass";
+        $classtoload->new_with_options->run;
+    }
 }
 1;
