db2a0513cf68e24219cc31378e31bd38e803dc43
Devin Austin <devin.austin@gmail.com>
2009-08-04 00:22:27 +0000

    split helper into app generation helper and component generation helper (initial split, at least)

---
+++ lib/Catalyst/Helper/AppGen.pm
@@ -0,0 +1,7 @@
+package Catalyst::Helper::AppGen;
+
+use Moose;
+use namespace::autoclean;
+extends { 'Catalyst::Helper' };
+
+1;
---
+++ lib/Catalyst/Helper/ComponentGen.pm
@@ -0,0 +1,6 @@
+package Catalyst::Helper::ComponentGen;
+use Moose;
+use namespace::autoclean;
+extends { 'Catalyst::Helper' };
+
+1;
