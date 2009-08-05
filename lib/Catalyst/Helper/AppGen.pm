9fb12b36f35749a1f5ef603562f1a3aee70db5f0
Devin Austin <devin.austin@gmail.com>
2009-08-05 20:07:12 +0000

    had this stuff in the wrong branch, blech

--- lib/Catalyst/Helper/AppGen.pm
+++
@@ -1,7 +0,0 @@
-package Catalyst::Helper::AppGen;
-
-use Moose;
-use namespace::autoclean;
-extends { 'Catalyst::Helper' };
-
-1;
--- lib/Catalyst/Helper/ComponentGen.pm
+++
@@ -1,6 +0,0 @@
-package Catalyst::Helper::ComponentGen;
-use Moose;
-use namespace::autoclean;
-extends { 'Catalyst::Helper' };
-
-1;
