e65d000f93c6cadddf23131bd0fd04f5f114fe7a
Tomas Doran <bobtfish@bobtfish.net>
2009-08-11 21:49:37 +0000

    Unfuck Catalyst::View::JSON

--- lib/Catalyst/Component.pm
+++ lib/Catalyst/Component.pm
@@ -64,9 +64,12 @@ has _component_name => ( is => 'ro' ); # Cannot be required => 1 as context
                                        # class @ISA component - HATE
 # Make accessor callable as a class method, as we need to call setup_actions
 # on the application class, which we don't have an instance of, ewwwww
+# Also, naughty modules like Catalyst::View::JSON try to write to _everything_,
+# so spit a warning, ignore that (and try to do the right thing anyway) here..
 around _component_name => sub {
     my ($orig, $self) = (shift, shift);
-    blessed($self) ? $self->$orig(@_) : $self;
+    Carp::cluck("Tried to write to the _component_name accessor - is your component broken or just mad? (Write ignored - using default value.)") if scalar @_;
+    blessed($self) ? $self->$orig() || blessed($self) : $self;
 };
 
 sub BUILDARGS {
