cb88f828a7fa6a90b46d1c7bca2d116820ea3d04
Yousef H. Alhashemi <yousef.alhashemi@gmail.com>
2009-08-16 18:08:03 +0000

    Returned the foo() action to TestAppPathBug.pm so it tests the right bug. Added a signal handler to silence the warning. Moved action methods from TestAppStats.pm to a root controller.

--- t/lib/TestAppPathBug.pm
+++ t/lib/TestAppPathBug.pm
@@ -3,6 +3,8 @@ use warnings;
 
 package TestAppPathBug;
 
+BEGIN { $SIG{__WARN__} = sub {}; };
+
 use Catalyst;
 
 our $VERSION = '0.01';
@@ -10,5 +12,10 @@ our $VERSION = '0.01';
 __PACKAGE__->config( name => 'TestAppPathBug', root => '/some/dir' );
 
 __PACKAGE__->setup;
+
+sub foo : Path {
+    my ( $self, $c ) = @_;
+    $c->res->body( 'This is the foo method.' );
+}
 
 1;
