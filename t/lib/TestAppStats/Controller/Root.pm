f1abcb4a7a3374b2e400b33b9cf5c0c6a5d49734
Tomas Doran <bobtfish@bobtfish.net>
2009-08-19 19:03:01 +0000

    Move test to make more sense in my idea of the naming scheme - t/dead_ is for tests which check the app pukes, which this shouldn't

---
+++ t/lib/TestAppStats/Controller/Root.pm
@@ -0,0 +1,17 @@
+package TestAppStats::Controller::Root;
+use strict;
+use warnings;
+use base qw/Catalyst::Controller/;
+
+__PACKAGE__->config->{namespace} = '';
+
+# Return log messages from previous request
+sub default : Private {
+    my ( $self, $c ) = @_;
+    $c->stats->profile("test");
+    $c->res->body(join("\n", @TestAppStats::log_messages));
+    @TestAppStats::log_messages = ();
+}
+
+1;
+
