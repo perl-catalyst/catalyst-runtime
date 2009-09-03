5ec24151d10209c4bf8b4522e515dd775b51fa9c
Yousef H. Alhashemi <yousef.alhashemi@gmail.com>
2009-09-03 12:08:43 +0000

    Test that warnings are generated if appclass actions are used

--- t/deprecated_appclass_action_warnings.t
+++ t/deprecated_appclass_action_warnings.t
@@ -7,7 +7,12 @@ use lib "$FindBin::Bin/lib";
 use Test::More;
 use Catalyst::Test 'DeprecatedActionsInAppClassTestApp';
 
-plan tests => 2;
+plan tests => 3;
+
+my $warnings;
+my $logger = DeprecatedActionsInAppClassTestApp::Log->new;
+Catalyst->log($logger);
 
 ok( my $response = request('http://localhost/foo'), 'Request' );
 ok( $response->is_success, 'Response Successful 2xx' );
+is( $warnings, 1, 'Get the appclass action warning' );
--- t/lib/DeprecatedActionsInAppClassTestApp.pm
+++ t/lib/DeprecatedActionsInAppClassTestApp.pm
@@ -4,11 +4,25 @@ use strict;
 use warnings;
 use Catalyst;
 
+our $VERSION = '0.01';
+
+__PACKAGE__->config( name => 'DeprecatedActionsInAppClassTestApp', root => '/some/dir' );
+__PACKAGE__->log(DeprecatedActionsInAppClassTestApp::Log->new);
 __PACKAGE__->setup;
 
 sub foo : Local {
     my ($self, $c) = @_;
     $c->res->body('OK');
+}
+
+package DeprecatedActionsInAppClassTestApp::Log;
+use strict;
+use warnings;
+use base qw/Catalyst::Log/;
+
+sub warn {
+    my ($self, $warning) = @_;
+    $warnings++ if $warning =~ /action methods .+ found defined/i;
 }
 
 1;
