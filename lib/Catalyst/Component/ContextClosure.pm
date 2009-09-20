432c4971046be0d5e2e678371de4d40bf028981d
Florian Ragwitz <rafl@debian.org>
2009-09-20 14:01:45 +0000

    Fix a syntax error in the ContextClosure pod.

--- lib/Catalyst/Component/ContextClosure.pm
+++ lib/Catalyst/Component/ContextClosure.pm
@@ -33,7 +33,7 @@ Catalyst::Component::ContextClosure - Moose Role for components which need to cl
         $ctx->stash(a_closure => $self->make_context_closure(sub {
             my ($ctx) = @_;
             $ctx->response->body('body set from closure');
-        }, $ctx);
+        }, $ctx));
     }
 
 =head1 DESCRIPTION
