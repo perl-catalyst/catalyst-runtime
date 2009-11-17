8458a66a25f3bd41bc9b6f13a1cd59000a2da411
Zbigniew Łukasiak <zbigniew@lukasiak.name>
2009-11-17 14:54:07 +0000

    now prepare_action is in Context - fixing test libs (for example for test t/aggregate/live_component_controller_action_action.t)

--- t/lib/TestApp/Context.pm
+++ t/lib/TestApp/Context.pm
@@ -51,6 +51,10 @@ sub execute {
     return $c->SUPER::execute(@_);
 }
 
+after prepare_action => sub{
+    my $c = shift;
+    $c->res->header( 'X-Catalyst-Action' => $c->req->action );
+};
 
 1;
 
