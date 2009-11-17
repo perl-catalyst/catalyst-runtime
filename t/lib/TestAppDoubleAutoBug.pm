a478032e68d51c107b3132aaddccf4d664637d9f
Zbigniew Łukasiak <zbigniew@lukasiak.name>
2009-11-17 15:21:01 +0000

    separate context class for TestAppDoubleAutoBug

--- t/lib/TestAppDoubleAutoBug.pm
+++ t/lib/TestAppDoubleAutoBug.pm
@@ -9,12 +9,12 @@ use Catalyst qw/
     Test::Plugin
 /;
 
-use TestApp::Context;
+use TestAppDoubleAutoBug::Context;
 
 our $VERSION = '0.01';
 
 __PACKAGE__->config( name => 'TestAppDoubleAutoBug', root => '/some/dir' );
-__PACKAGE__->context_class( 'TestApp::Context' );
+__PACKAGE__->context_class( 'TestAppDoubleAutoBug::Context' );
 __PACKAGE__->setup;
 
 1;
---
+++ t/lib/TestAppDoubleAutoBug/Context.pm
@@ -0,0 +1,60 @@
+package TestAppDoubleAutoBug::Context;
+use Moose;
+extends 'Catalyst::Context'; 
+
+if (eval { Class::MOP::load_class('CatalystX::LeakChecker'); 1 }) {
+    with 'CatalystX::LeakChecker';
+
+    has leaks => (
+        is      => 'ro',
+        default => sub { [] },
+    );
+}
+
+sub found_leaks {
+    my ($ctx, @leaks) = @_;
+    push @{ $ctx->leaks }, @leaks;
+}
+
+sub count_leaks {
+    my ($ctx) = @_;
+    return scalar @{ $ctx->leaks };
+}
+
+sub execute {
+    my $c      = shift;
+    my $class  = ref( $c->component( $_[0] ) ) || $_[0];
+    my $action = $_[1]->reverse;
+
+    my $method;
+
+    if ( $action =~ /->(\w+)$/ ) {
+        $method = $1;
+    }
+    elsif ( $action =~ /\/(\w+)$/ ) {
+        $method = $1;
+    }
+    elsif ( $action =~ /^(\w+)$/ ) {
+        $method = $action;
+    }
+
+    if ( $class && $method && $method !~ /^_/ ) {
+        my $executed = sprintf( "%s->%s", $class, $method );
+        my @executed = $c->response->headers->header('X-Catalyst-Executed');
+        push @executed, $executed;
+        $c->response->headers->header(
+            'X-Catalyst-Executed' => join ', ',
+            @executed
+        );
+    }
+    no warnings 'recursion';
+    return $c->SUPER::execute(@_);
+}
+
+after prepare_action => sub{
+    my $c = shift;
+    $c->res->header( 'X-Catalyst-Action' => $c->req->action );
+};
+
+1;
+
