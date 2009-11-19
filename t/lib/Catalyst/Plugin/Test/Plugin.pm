a00f6df35924b5f9c96b94f6ecaadae1b8826400
Zbigniew Łukasiak <zbigniew@lukasiak.name>
2009-11-19 21:54:32 +0000

    fix for inheritance mismatch

--- t/lib/Catalyst/Plugin/Test/Plugin.pm
+++ t/lib/Catalyst/Plugin/Test/Plugin.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use MRO::Compat;
 
-use base qw/Catalyst::Controller Class::Data::Inheritable/;
+use base qw/Class::Data::Inheritable/;
 
  __PACKAGE__->mk_classdata('ran_setup');
 
