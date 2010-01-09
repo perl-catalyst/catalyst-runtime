bb55391fcb8b0bac4e35ae0ed9a2425cccf234fc
Tomas Doran <bobtfish@bobtfish.net>
2010-01-09 15:57:27 +0000

    Back out r12493, use \Q instead

--- lib/Catalyst/Engine/CGI.pm
+++ lib/Catalyst/Engine/CGI.pm
@@ -167,8 +167,7 @@ sub prepare_path {
             # incorrect.
             if (substr($req_uri, 0, 1) ne '/') {
                 my ($match) = $req_uri =~ m|^([^/]+)|;
-                my $idx = index($path_info, $match) + length($match);
-                my $path_info_part = substr($path_info, 0, $idx);
+                my ($path_info_part) = $path_info =~ m|^(.*?\Q$match)|;
                 substr($req_uri, 0, length($match), $path_info_part);
             }
             $path_info = $req_uri;
