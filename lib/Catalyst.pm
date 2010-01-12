c6e5058f2acfe2ef1f6961191b1961db1d0295e4
Florian Ragwitz <rafl@debian.org>
2010-01-12 21:37:46 +0000

    Version 5.80018.

--- lib/Catalyst.pm
+++ lib/Catalyst.pm
@@ -78,7 +78,7 @@ __PACKAGE__->stats_class('Catalyst::Stats');
 
 # Remember to update this in Catalyst::Runtime as well!
 
-our $VERSION = '5.80017';
+our $VERSION = '5.80018';
 $VERSION = eval $VERSION;
 
 sub import {
