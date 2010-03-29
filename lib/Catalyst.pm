14a1ceeffe74d8c24dc270238f4069915270a6e5
Tomas Doran <bobtfish@bobtfish.net>
2010-03-29 15:55:39 +0000

    Fail, commit version bump

--- lib/Catalyst.pm
+++ lib/Catalyst.pm
@@ -78,7 +78,7 @@ __PACKAGE__->stats_class('Catalyst::Stats');
 
 # Remember to update this in Catalyst::Runtime as well!
 
-our $VERSION = '5.80021';
+our $VERSION = '5.80022';
 our $PRETTY_VERSION = $VERSION;
 
 $VERSION = eval $VERSION;
--- lib/Catalyst/Runtime.pm
+++ lib/Catalyst/Runtime.pm
@@ -7,7 +7,7 @@ BEGIN { require 5.008004; }
 
 # Remember to update this in Catalyst as well!
 
-our $VERSION='5.80021';
+our $VERSION='5.80022';
 
 $VERSION = eval $VERSION;
 
