10e970e4efdf69f9cd023b93016514f7668b54f7
Tomas Doran <bobtfish@bobtfish.net>
2010-06-15 21:17:32 +0000

    Commit standalone test, test which makes the TestApp blow up and the nasty workaround

--- lib/Catalyst/Controller.pm
+++ lib/Catalyst/Controller.pm
@@ -225,7 +225,7 @@ sub register_action_methods {
 
     foreach my $method (@methods) {
         my $name = $method->name;
-        my $attributes = $method->attributes;
+        my $attributes = $method->can('attributes') ? $method->attributes : [];
         my $attrs = $self->_parse_attrs( $c, $name, @{ $attributes } );
         if ( $attrs->{Private} && ( keys %$attrs > 1 ) ) {
             $c->log->debug( 'Bad action definition "'
