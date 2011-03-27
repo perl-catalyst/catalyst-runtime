cac8fd6e3d729956b956f20aa0b3e2a1c5c83fd3
Tomas Doran <bobtfish@bobtfish.net>
2011-03-27 16:54:11 +0100

    Force FCGI detection for FastCGI scripts.

--- lib/Catalyst/EngineLoader.pm
+++ lib/Catalyst/EngineLoader.pm
@@ -82,8 +82,11 @@ around guess => sub {
     my $old_engine = Catalyst::Utils::env_value($self->application_name, 'ENGINE');
     if (!defined $old_engine) { # Not overridden
     }
-    elsif ($old_engine =~ /^(PSGI|CGI|FastCGI|HTTP|Apache.*)$/) {
+    elsif ($old_engine =~ /^(PSGI|CGI|HTTP|Apache.*)$/) {
         # Trust autodetect
+    }
+    elsif ($old_engine eq 'FastCGI') {
+        $engine = 'FCGI';
     }
     elsif ($old_engine eq "HTTP::Prefork") { # Too bad if you're customising, we don't handle options
                                              # write yourself a script to collect and pass in the options
