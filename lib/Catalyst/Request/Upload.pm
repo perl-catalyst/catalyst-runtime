554c758740d1f5b5b548b777e7180e556f9f6a55
Florian Ragwitz <rafl@debian.org>
2011-04-14 14:09:13 +0200

    Unbreak the pod-coverage tests
    Without removing those imports, it expects us to document them.

--- lib/Catalyst/Request/Upload.pm
+++ lib/Catalyst/Request/Upload.pm
@@ -7,6 +7,7 @@ use Catalyst::Exception;
 use File::Copy ();
 use IO::File   qw( SEEK_SET );
 use File::Spec::Unix;
+use namespace::clean -except => 'meta';
 
 has filename => (is => 'rw');
 has headers => (is => 'rw');
