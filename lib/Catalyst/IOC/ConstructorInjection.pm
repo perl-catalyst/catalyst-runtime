637cebbe87f49e1fe2309ac039af32eb9330aff9
André Walker <andre@andrewalker.net>
2011-07-29 01:01:05 -0300

    removed code from WithCOMPONENT

--- lib/Catalyst/IOC/ConstructorInjection.pm
+++ lib/Catalyst/IOC/ConstructorInjection.pm
@@ -5,10 +5,10 @@ extends 'Bread::Board::ConstructorInjection';
 with 'Bread::Board::Service::WithClass',
      'Bread::Board::Service::WithDependencies',
      'Bread::Board::Service::WithParameters',
+     'Catalyst::IOC::Service::WithCOMPONENT',
      'Catalyst::IOC::Service::WithAcceptContext';
 
-sub _build_constructor_name { 'COMPONENT' }
-
+# FIXME - how much of this should move to ::WithCOMPONENT?
 sub get {
     my $self = shift;
 
@@ -18,36 +18,12 @@ sub get {
     my $config      = $params->{config}->{ $self->param('suffix') } || {};
     my $app_name    = $params->{application_name};
 
-    unless ( $component->can( $constructor ) ) {
-        # FIXME - make some deprecation warnings
-        return $component;
-    }
-
     # Stash catalyst_component_name in the config here, so that custom COMPONENT
     # methods also pass it. local to avoid pointlessly shitting in config
     # for the debug screen, as $component is already the key name.
     local $config->{catalyst_component_name} = $component;
 
-    my $instance = eval { $component->$constructor( $app_name, $config ) };
-
-    if ( my $error = $@ ) {
-        chomp $error;
-        Catalyst::Exception->throw(
-            message => qq/Couldn't instantiate component "$component", "$error"/
-        );
-    }
-    elsif (!blessed $instance) {
-        my $metaclass = Moose::Util::find_meta($component);
-        my $method_meta = $metaclass->find_method_by_name('COMPONENT');
-        my $component_method_from = $method_meta->associated_metaclass->name;
-        my $value = defined($instance) ? $instance : 'undef';
-        Catalyst::Exception->throw(
-            message =>
-            qq/Couldn't instantiate component "$component", COMPONENT() method (from $component_method_from) didn't return an object-like value (value was $value)./
-        );
-    }
-
-    return $instance;
+    return $component->$constructor( $app_name, $config );
 }
 
 __PACKAGE__->meta->make_immutable;
