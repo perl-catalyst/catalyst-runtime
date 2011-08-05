51f62e09088a7fbdbbe53a76681e1a51e06d328a
André Walker <andre@andrewalker.net>
2011-08-05 17:51:08 -0300

    Basing Catalyst::IOC on Bread::Board (don't know if it's supposed to, but current container tests require it) and pod sketch

--- lib/Catalyst/IOC.pm
+++ lib/Catalyst/IOC.pm
@@ -1,8 +1,34 @@
 package Catalyst::IOC;
 use strict;
 use warnings;
+use base 'Bread::Board';
 
 # You want to use Sub::Exporter here to export our sugar (and probably some of
 # the bread::board sugar
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Catalyst::IOC - IOC for Catalyst, based on Bread::Board
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head1 AUTHORS
+
+Catalyst Contributors, see Catalyst.pm
+
+=head1 COPYRIGHT
+
+This library is free software. You can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
