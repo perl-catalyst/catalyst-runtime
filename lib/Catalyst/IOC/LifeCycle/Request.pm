64e06b7d571fe1c845df839a19ae781cd256138c
André Walker <andre@andrewalker.net>
2011-08-05 17:52:43 -0300

    pod sketch for Request lifecycle

--- lib/Catalyst/IOC/LifeCycle/Request.pm
+++ lib/Catalyst/IOC/LifeCycle/Request.pm
@@ -8,3 +8,28 @@ use namespace::autoclean;
 with 'Bread::Board::LifeCycle::Singleton';
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Catalyst::IOC::LifeCycle::Request - Components that last for one request
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head1 AUTHORS
+
+Catalyst Contributors, see Catalyst.pm
+
+=head1 COPYRIGHT
+
+This library is free software. You can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
