df0d84419ff21493fe7afd477f0201ce5590fdf1
André Walker <andre@andrewalker.net>
2011-08-10 00:55:39 -0300

    ditching the COMPONENT constructor

--- t/lib/TestAppCustomContainer/Model/Bar.pm
+++ t/lib/TestAppCustomContainer/Model/Bar.pm
@@ -4,6 +4,12 @@ extends 'Catalyst::Model';
 with 'TestAppCustomContainer::Role::HoldsFoo',
      'TestAppCustomContainer::Role::ACCEPT_CONTEXT';
 
+sub BUILD {
+    my ( $self ) = @_;
+
+    $self->foo->inc_bar_got_it;
+}
+
 __PACKAGE__->meta->make_immutable;
 
 no Moose;
--- t/lib/TestAppCustomContainer/Role/HoldsFoo.pm
+++ t/lib/TestAppCustomContainer/Role/HoldsFoo.pm
@@ -8,13 +8,4 @@ has foo => (
     required => 1,
 );
 
-sub COMPONENT {
-    my ( $self, $ctx, $config ) = @_;
-
-    # FIXME - is this how should I get model Foo?
-    return $self->new(
-        foo => $ctx->model('Foo'),
-    );
-}
-
 1;
