3143ee5f686e48785bc13cf756a4dde97aae682e
Tomas Doran <bobtfish@bobtfish.net>
2011-08-15 22:45:03 +0100

    Version 5.90001. Correct no of digits in VERSION - DOH

--- lib/Catalyst.pm
+++ lib/Catalyst.pm
@@ -84,7 +84,7 @@ __PACKAGE__->stats_class('Catalyst::Stats');
 
 # Remember to update this in Catalyst::Runtime as well!
 
-our $VERSION = '5.9000';
+our $VERSION = '5.90001';
 
 sub import {
     my ( $class, @arguments ) = @_;
--- lib/Catalyst/Runtime.pm
+++ lib/Catalyst/Runtime.pm
@@ -7,7 +7,7 @@ BEGIN { require 5.008004; }
 
 # Remember to update this in Catalyst as well!
 
-our $VERSION = '5.9000';
+our $VERSION = '5.90001';
 
 =head1 NAME
 
