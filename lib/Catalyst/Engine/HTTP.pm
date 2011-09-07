f1446902707bffd041e9c33775c47453b7614f39
Tomas Doran <bobtfish@bobtfish.net>
2011-09-07 19:25:04 +0100

    Remember to add deprecation notice

--- lib/Catalyst/Engine/HTTP.pm
+++ lib/Catalyst/Engine/HTTP.pm
@@ -5,6 +5,17 @@ use warnings;
 
 use base 'Catalyst::Engine';
 
+warn("You are loading Catalyst::Engine::HTTP explicitly.
+
+This is almost certainally a bad idea, as Catalyst::Engine::HTTP
+has been removed in this version of Catalyst.
+
+Please update your application's scripts with:
+
+  catalyst.pl -force -scripts MyApp
+
+to update your scripts to not do this.\n");
+
 1;
 
 # This is here only as some old generated scripts require Catalyst::Engine::HTTP
