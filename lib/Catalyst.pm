849b1b24399427de46fc460c1f87db8508e99535
Tomas Doran <bobtfish@bobtfish.net>
2011-10-22 12:35:35 -0700

    Version 5.90005

--- lib/Catalyst.pm
+++ lib/Catalyst.pm
@@ -84,7 +84,7 @@ __PACKAGE__->stats_class('Catalyst::Stats');
 
 # Remember to update this in Catalyst::Runtime as well!
 
-our $VERSION = '5.90004';
+our $VERSION = '5.90005';
 
 sub import {
     my ( $class, @arguments ) = @_;
