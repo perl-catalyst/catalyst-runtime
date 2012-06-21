aa3897dbd07156dbd1b80c5d1571101140e13a11
John Napiorkowski <jjn1056@yahoo.com>
2012-06-21 10:41:18 -0400

    updated changes and version for new release

--- lib/Catalyst.pm
+++ lib/Catalyst.pm
@@ -100,7 +100,7 @@ __PACKAGE__->stats_class('Catalyst::Stats');
 
 # Remember to update this in Catalyst::Runtime as well!
 
-our $VERSION = '5.90012';
+our $VERSION = '5.90013';
 
 sub import {
     my ( $class, @arguments ) = @_;
