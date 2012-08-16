f0580bff341c3d9a8a62bb8e670ff8772745786f
John Napiorkowski <jjn1056@yahoo.com>
2012-08-16 15:36:57 -0400

    claim a version for release

--- lib/Catalyst.pm
+++ lib/Catalyst.pm
@@ -102,7 +102,7 @@ __PACKAGE__->stats_class('Catalyst::Stats');
 
 # Remember to update this in Catalyst::Runtime as well!
 
-our $VERSION = '5.90015';
+our $VERSION = '5.90016';
 
 sub import {
     my ( $class, @arguments ) = @_;
