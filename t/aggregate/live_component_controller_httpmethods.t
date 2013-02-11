760d121ecfcc1ad7c28342a6912a472fe30e5fe0
John Napiorkowski <jjnapiork@cpan.org>
2013-02-11 17:22:07 -0500

    new match and match captutres for http methods, plus tests, docs

---
+++ t/aggregate/live_component_controller_httpmethods.t
@@ -0,0 +1,38 @@
+use strict;
+use warnings;
+use Test::More;
+use HTTP::Request::Common qw/GET POST DELETE PUT /;
+ 
+use FindBin;
+use lib "$FindBin::Bin/../lib";
+
+use Catalyst::Test 'TestApp';
+ 
+is(request(GET    '/httpmethods/foo')->content, 'get');
+is(request(POST   '/httpmethods/foo')->content, 'post');
+is(request(DELETE '/httpmethods/foo')->content, 'default');
+ 
+is(request(GET    '/httpmethods/bar')->content, 'get or post');
+is(request(POST   '/httpmethods/bar')->content, 'get or post');
+is(request(DELETE '/httpmethods/bar')->content, 'default');
+ 
+is(request(GET    '/httpmethods/baz')->content, 'any');
+is(request(POST   '/httpmethods/baz')->content, 'any');
+is(request(DELETE '/httpmethods/baz')->content, 'any');
+
+is(request(GET    '/httpmethods/chained_get')->content, 'chained_get');
+is(request(POST    '/httpmethods/chained_post')->content, 'chained_post');
+is(request(PUT    '/httpmethods/chained_put')->content, 'chained_put');
+is(request(DELETE    '/httpmethods/chained_delete')->content, 'chained_delete');
+
+is(request(GET '/httpmethods/get_put_post_delete')->content, 'get2');
+is(request(POST '/httpmethods/get_put_post_delete')->content, 'post2');
+is(request(PUT '/httpmethods/get_put_post_delete')->content, 'put2');
+is(request(DELETE '/httpmethods/get_put_post_delete')->content, 'delete2');
+
+is(request(GET '/httpmethods/check_default')->content, 'get3');
+is(request(POST '/httpmethods/check_default')->content, 'post3');
+is(request(PUT '/httpmethods/check_default')->content, 'chain_default');
+
+
+done_testing;
---
+++ t/lib/TestApp/Controller/HTTPMethods.pm
@@ -0,0 +1,88 @@
+package TestApp::Controller::HTTPMethods;
+
+use Moose;
+use MooseX::MethodAttributes;
+ 
+extends 'Catalyst::Controller';
+ 
+sub default : Path Args {
+    my ($self, $ctx) = @_;
+    $ctx->response->body('default');
+}
+ 
+sub get : Path('foo') Method('GET') {
+    my ($self, $ctx) = @_;
+    $ctx->response->body('get');
+}
+ 
+sub post : Path('foo') Method('POST') {
+    my ($self, $ctx) = @_;
+    $ctx->response->body('post');
+}
+ 
+sub get_or_post : Path('bar') Method('GET') Method('POST') {
+    my ($self, $ctx) = @_;
+    $ctx->response->body('get or post');
+}
+ 
+sub any_method : Path('baz') {
+    my ($self, $ctx) = @_;
+    $ctx->response->body('any');
+}
+
+sub base :Chained('/') PathPrefix CaptureArgs(0) { }
+
+  sub chained_get :Chained('base') Args(0) GET {
+    pop->res->body('chained_get');
+  }
+
+  sub chained_post :Chained('base') Args(0) POST {
+    pop->res->body('chained_post');
+  }
+
+  sub chained_put :Chained('base') Args(0) PUT {
+    pop->res->body('chained_put');
+  }
+
+  sub chained_delete :Chained('base') Args(0) DELETE {
+    pop->res->body('chained_delete');
+  }
+
+  sub get_or_put :Chained('base') PathPart('get_put_post_delete')
+    : CaptureArgs(0) GET PUT { }
+
+    sub get2 :Chained('get_or_put') PathPart('') Args(0) GET {
+      pop->res->body('get2');
+    }
+    
+    sub put2 :Chained('get_or_put') PathPart('') Args(0) PUT {
+      pop->res->body('put2');
+    }
+
+  sub post_or_delete :Chained('base') PathPart('get_put_post_delete')
+    : CaptureArgs(0) POST DELETE { }
+
+    sub post2 :Chained('post_or_delete') PathPart('') Args(0) POST {
+      pop->res->body('post2');
+    }
+    
+    sub delete2 :Chained('post_or_delete') PathPart('') Args(0) DELETE {
+      pop->res->body('delete2');
+    }
+
+  sub check_default :Chained('base') CaptureArgs(0) { }
+
+    sub default_get :Chained('check_default') PathPart('') Args(0) GET {
+      pop->res->body('get3');
+    }
+
+    sub default_post :Chained('check_default') PathPart('') Args(0) POST {
+      pop->res->body('post3');
+    }
+
+    sub chain_default :Chained('check_default') PathPart('') Args(0) {
+      pop->res->body('chain_default');
+    }
+
+
+__PACKAGE__->meta->make_immutable;
