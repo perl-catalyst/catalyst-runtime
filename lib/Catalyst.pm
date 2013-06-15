8dfa6541f3a31907a994eb7d8337315073342a65
John Napiorkowski <jjnapiork@cpan.org>
2013-06-15 10:00:26 -0400

    update changlog and version in prep for release

--- lib/Catalyst.pm
+++ lib/Catalyst.pm
@@ -113,7 +113,7 @@ __PACKAGE__->stats_class('Catalyst::Stats');
 
 # Remember to update this in Catalyst::Runtime as well!
 
-our $VERSION = '5.90041';
+our $VERSION = '5.90042';
 
 sub import {
     my ( $class, @arguments ) = @_;
