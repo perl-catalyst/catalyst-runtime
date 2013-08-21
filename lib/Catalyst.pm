e78db0df0d5a5e9f7c4c2186433e00c23c531c36
John Napiorkowski <jjnapiork@cpan.org>
2013-08-20 22:31:09 -0400

    prepping changefile and version info

--- lib/Catalyst.pm
+++ lib/Catalyst.pm
@@ -117,7 +117,7 @@ __PACKAGE__->stats_class('Catalyst::Stats');
 
 # Remember to update this in Catalyst::Runtime as well!
 
-our $VERSION = '5.90049_001';
+our $VERSION = '5.90049_002';
 
 sub import {
     my ( $class, @arguments ) = @_;
--- lib/Catalyst/Runtime.pm
+++ lib/Catalyst/Runtime.pm
@@ -7,7 +7,7 @@ BEGIN { require 5.008003; }
 
 # Remember to update this in Catalyst as well!
 
-our $VERSION = '5.90049_001';
+our $VERSION = '5.90049_002';
 
 =head1 NAME
 
