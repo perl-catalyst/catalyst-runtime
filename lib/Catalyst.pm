a74d53e34e33a6252905b25f93a8a7359e1fb5d1
John Napiorkowski <jjnapiork@cpan.org>
2013-12-21 14:48:11 -0600

    tag the next dev version to make it easier to locally install

--- lib/Catalyst.pm
+++ lib/Catalyst.pm
@@ -122,7 +122,7 @@ __PACKAGE__->stats_class('Catalyst::Stats');
 
 # Remember to update this in Catalyst::Runtime as well!
 
-our $VERSION = '5.90059_001';
+our $VERSION = '5.90059_002';
 
 sub import {
     my ( $class, @arguments ) = @_;
