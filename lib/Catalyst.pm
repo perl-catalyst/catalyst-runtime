cbbd38c38f134d5540ed1b422b000e163985f4fc
Neil Lunn <neil@mylunn.id.au>
2014-02-11 11:11:52 +1100

    Move $c->welcome_message to Plugin
    * Pull out $c->welcome_messgage and move to Catalyst::Plugin::Welcome
    * Welcome plugin added to the default_plugins
    * Add default minimal sub if Plugin removed from core

--- lib/Catalyst.pm
+++ lib/Catalyst.pm
@@ -1491,153 +1491,16 @@ sub uri_for_action {
     return $c->uri_for( $action, @args );
 }
 
-=head2 $c->welcome_message
+=head1 $c->welcome_message
 
-Returns the Catalyst welcome HTML page.
+Basic response. See L<Catalyst::Plugin::Welcome> for the old page.
 
 =cut
 
 sub welcome_message {
-    my $c      = shift;
-    my $name   = $c->config->{name};
-    my $logo   = $c->uri_for('/static/images/catalyst_logo.png');
-    my $prefix = Catalyst::Utils::appprefix( ref $c );
-    $c->response->content_type('text/html; charset=utf-8');
-    return <<"EOF";
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-    <head>
-    <meta http-equiv="Content-Language" content="en" />
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>$name on Catalyst $VERSION</title>
-        <style type="text/css">
-            body {
-                color: #000;
-                background-color: #eee;
-            }
-            div#content {
-                width: 640px;
-                margin-left: auto;
-                margin-right: auto;
-                margin-top: 10px;
-                margin-bottom: 10px;
-                text-align: left;
-                background-color: #ccc;
-                border: 1px solid #aaa;
-            }
-            p, h1, h2 {
-                margin-left: 20px;
-                margin-right: 20px;
-                font-family: verdana, tahoma, sans-serif;
-            }
-            a {
-                font-family: verdana, tahoma, sans-serif;
-            }
-            :link, :visited {
-                    text-decoration: none;
-                    color: #b00;
-                    border-bottom: 1px dotted #bbb;
-            }
-            :link:hover, :visited:hover {
-                    color: #555;
-            }
-            div#topbar {
-                margin: 0px;
-            }
-            pre {
-                margin: 10px;
-                padding: 8px;
-            }
-            div#answers {
-                padding: 8px;
-                margin: 10px;
-                background-color: #fff;
-                border: 1px solid #aaa;
-            }
-            h1 {
-                font-size: 0.9em;
-                font-weight: normal;
-                text-align: center;
-            }
-            h2 {
-                font-size: 1.0em;
-            }
-            p {
-                font-size: 0.9em;
-            }
-            p img {
-                float: right;
-                margin-left: 10px;
-            }
-            span#appname {
-                font-weight: bold;
-                font-size: 1.6em;
-            }
-        </style>
-    </head>
-    <body>
-        <div id="content">
-            <div id="topbar">
-                <h1><span id="appname">$name</span> on <a href="http://catalyst.perl.org">Catalyst</a>
-                    $VERSION</h1>
-             </div>
-             <div id="answers">
-                 <p>
-                 <img src="$logo" alt="Catalyst Logo" />
-                 </p>
-                 <p>Welcome to the  world of Catalyst.
-                    This <a href="http://en.wikipedia.org/wiki/MVC">MVC</a>
-                    framework will make web development something you had
-                    never expected it to be: Fun, rewarding, and quick.</p>
-                 <h2>What to do now?</h2>
-                 <p>That really depends  on what <b>you</b> want to do.
-                    We do, however, provide you with a few starting points.</p>
-                 <p>If you want to jump right into web development with Catalyst
-                    you might want to start with a tutorial.</p>
-<pre>perldoc <a href="https://metacpan.org/module/Catalyst::Manual::Tutorial">Catalyst::Manual::Tutorial</a></code>
-</pre>
-<p>Afterwards you can go on to check out a more complete look at our features.</p>
-<pre>
-<code>perldoc <a href="https://metacpan.org/module/Catalyst::Manual::Intro">Catalyst::Manual::Intro</a>
-<!-- Something else should go here, but the Catalyst::Manual link seems unhelpful -->
-</code></pre>
-                 <h2>What to do next?</h2>
-                 <p>Next it's time to write an actual application. Use the
-                    helper scripts to generate <a href="https://metacpan.org/search?q=Catalyst%3A%3AController">controllers</a>,
-                    <a href="https://metacpan.org/search?q=Catalyst%3A%3AModel">models</a>, and
-                    <a href="https://metacpan.org/search?q=Catalyst%3A%3AView">views</a>;
-                    they can save you a lot of work.</p>
-                    <pre><code>script/${prefix}_create.pl --help</code></pre>
-                    <p>Also, be sure to check out the vast and growing
-                    collection of <a href="http://search.cpan.org/search?query=Catalyst">plugins for Catalyst on CPAN</a>;
-                    you are likely to find what you need there.
-                    </p>
-
-                 <h2>Need help?</h2>
-                 <p>Catalyst has a very active community. Here are the main places to
-                    get in touch with us.</p>
-                 <ul>
-                     <li>
-                         <a href="http://dev.catalyst.perl.org">Wiki</a>
-                     </li>
-                     <li>
-                         <a href="http://lists.scsys.co.uk/cgi-bin/mailman/listinfo/catalyst">Mailing-List</a>
-                     </li>
-                     <li>
-                         <a href="irc://irc.perl.org/catalyst">IRC channel #catalyst on irc.perl.org</a>
-                     </li>
-                 </ul>
-                 <h2>In conclusion</h2>
-                 <p>The Catalyst team hopes you will enjoy using Catalyst as much
-                    as we enjoyed making it. Please contact us if you have ideas
-                    for improvement or other feedback.</p>
-             </div>
-         </div>
-    </body>
-</html>
-EOF
+  return "Welcome";
 }
+
 
 =head2 run_options
 
@@ -3019,7 +2882,7 @@ the plugin name does not begin with C<Catalyst::Plugin::>.
         return $class;
     }
 
-    sub _default_plugins { return qw(Unicode::Encoding) }
+    sub _default_plugins { return qw(Unicode::Encoding Welcome) }
 
     sub setup_plugins {
         my ( $class, $plugins ) = @_;
