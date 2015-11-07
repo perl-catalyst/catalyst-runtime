9045f5a10d8705a83c20d709523e321b9df8630f
John Napiorkowski <jjn1056@yahoo.com>
2015-11-06 18:46:21 -0600

    prep for release

--- lib/Catalyst.pm
+++ lib/Catalyst.pm
@@ -180,7 +180,7 @@ sub composed_stats_class {
 __PACKAGE__->_encode_check(Encode::FB_CROAK | Encode::LEAVE_SRC);
 
 # Remember to update this in Catalyst::Runtime as well!
-our $VERSION = '5.90102';
+our $VERSION = '5.90103';
 $VERSION = eval $VERSION if $VERSION =~ /_/; # numify for warning-free dev releases
 
 sub import {
