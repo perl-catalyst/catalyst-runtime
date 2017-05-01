9d8747f59cb86f0a072273d6b3a270140bfdb0f3
John Napiorkowski <jjn1056@yahoo.com>
2017-05-01 11:35:12 -0500

    versions

--- lib/Catalyst/Runtime.pm
+++ lib/Catalyst/Runtime.pm
@@ -7,7 +7,7 @@ BEGIN { require 5.008003; }
 
 # Remember to update this in Catalyst as well!
 
-our $VERSION = '5.90114';
+our $VERSION = '5.90115';
 $VERSION = eval $VERSION if $VERSION =~ /_/; # numify for warning-free dev releases
 
 =head1 NAME
