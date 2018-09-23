6812960c92992af09f1b0c8ff37b5fcab11be24e
Karen Etheridge <ether@cpan.org>
2018-09-23 16:58:42 -0700

    Bumping version to 5.90119

--- lib/Catalyst/Plugin/Unicode/Encoding.pm
+++ lib/Catalyst/Plugin/Unicode/Encoding.pm
@@ -1,6 +1,6 @@
 package Catalyst::Plugin::Unicode::Encoding;
 
-our $VERSION = '5.90118';
+our $VERSION = '5.90119';
 
 1;
 
--- lib/Catalyst/Runtime.pm
+++ lib/Catalyst/Runtime.pm
@@ -7,7 +7,7 @@ BEGIN { require 5.008003; }
 
 # Remember to update this in Catalyst as well!
 
-our $VERSION = '5.90118';
+our $VERSION = '5.90119';
 $VERSION = eval $VERSION if $VERSION =~ /_/; # numify for warning-free dev releases
 
 =head1 NAME
