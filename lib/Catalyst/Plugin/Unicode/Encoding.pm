8ab4e09d3537c1b3e4eb4ca489a5b1c35568a7d3
Graham Knop <haarg@haarg.org>
2018-10-22 21:37:57 +0200

    Bumping version to 5.90_121

--- lib/Catalyst/Plugin/Unicode/Encoding.pm
+++ lib/Catalyst/Plugin/Unicode/Encoding.pm
@@ -1,6 +1,6 @@
 package Catalyst::Plugin::Unicode::Encoding;
 
-our $VERSION = '5.90120';
+our $VERSION = '5.90_121';
 
 1;
 
--- lib/Catalyst/Runtime.pm
+++ lib/Catalyst/Runtime.pm
@@ -7,7 +7,7 @@ BEGIN { require 5.008003; }
 
 # Remember to update this in Catalyst as well!
 
-our $VERSION = '5.90120';
+our $VERSION = '5.90_121';
 $VERSION = eval $VERSION if $VERSION =~ /_/; # numify for warning-free dev releases
 
 =head1 NAME
